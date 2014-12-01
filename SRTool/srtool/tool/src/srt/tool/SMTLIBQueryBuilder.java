27961622bded1bc46b6bd328fce72e18f06ff245
Marcel <mpn10@doc.ic.ac.uk>
2014-12-01 22:02:25 +0000

    more fix

--- SRTool/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
+++ SRTool/srtool/tool/src/srt/tool/SMTLIBQueryBuilder.java
@@ -48,7 +48,7 @@ public class SMTLIBQueryBuilder {
 		// Add assertion checks.
 		int currentPropertyIndex = 0;
 		for (AssertStmt assertStmt : constraints.propertyNodes) {
-			String varName = asrtStmt.getName() != null ? asrtStmt.getName() : ("prop" + i++);
+			String varName = assertStmt.getName() != null ? assertStmt.getName() : ("prop" + i++);
 			query.append("(define-fun " + varName + " () (Bool) (not (tobool " + exprConverter.visit(assertStmt.getCondition()) + ")))\n");
 			currentPropertyIndex++;
 		}
